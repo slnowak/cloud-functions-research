6c8b647fa949ee1ebb14e866ba08dc39fdb7c2ab
Kamil Kulak <kulak.kam@gmail.com>
2017-07-29 18:22:00 +0200

    R scripts for hot-cold

---
+++ azure/hot-cold/results/coldAfterPython/draw.r
@@ -0,0 +1,17 @@
+library("ggplot2")
+library("scales")
+
+png(filename="graph.png", width=1280, height=1024)
+
+dat = read.csv("data.csv")
+dat$timestamp = as.POSIXct(dat$timestamp, format = "%Y-%m-%dT%H:%M:%S")
+
+ggplot(data=dat, aes(timestamp)) +
+geom_line(aes(y = percentile_duration_100)) +
+theme_bw() +
+scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
+scale_y_continuous(breaks=pretty_breaks(n=10)) +
+labs(x = "Czas wykonania zapytania HTTP (hh:mm:ss)", y = "Czas odpowiedzi (ms)") + 
+ggtitle("Czas wykonania funkcji po sesji warmup w języku Python (10RPS)")
+
+
