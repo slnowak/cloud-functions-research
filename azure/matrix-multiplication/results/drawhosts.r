533ff23f79936b82b878d7a016bb281d70e2ff8a
Slawomir Nowak <slawomir.nowak@uk.tesco.com>
2017-07-30 16:15:03 +0200

    More plots

--- azure/matrix-multiplication/results/drawhosts.r
+++ azure/matrix-multiplication/results/drawhosts.r
@@ -1,20 +1,21 @@
 library("ggplot2")
 library("scales")
 
-png(filename="test.png", width=1280, height=1024)
+png(filename="hosts.png", width=640, height=480)
 
 dat = read.csv("hosts.csv")
 dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
 
 ggplot(data=dat, aes(x=time, y=count, group=1)) +
-geom_line(colour="blue", size=0.5) +
-   scale_x_datetime(labels = date_format("%Y-%m-%dT%H:%M"), breaks = pretty_breaks(n=10)) +
+theme_bw() +
+geom_line(colour="black", size=0.5) +
+   scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
    scale_y_continuous(breaks=pretty_breaks(n=10)) +
    theme(plot.margin = unit(c(1,1,1,1), "cm")) +
-   labs(x="Data") +
-   labs(y="Liczba instancji funkcji") +
-   geom_area(aes(y=count))
-
+   labs(x="Czas (hh:mm)") +
+   labs(y="Liczba maszyn wirtualnych") +
+   geom_area(aes(y=count)) +
+ggtitle("Liczba maszyn wirtualnych, na których uruchamiane są funkcje")
 
 
 
--- azure/matrix-multiplication/results/drawpercentiles.r
+++ azure/matrix-multiplication/results/drawpercentiles.r
@@ -1,19 +1,20 @@
 library("ggplot2")
 library("scales")
 
-png(filename="perctest.png", width=1280, height=1024)
+png(filename="percentiles.png", width=640, height=480)
 
 dat = read.csv("percentiles.csv")
-dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
+dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%S")
 
 ggplot(data=dat, aes(time)) +
 theme_bw() +
 scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
 scale_y_continuous(breaks=pretty_breaks(n=10)) +
-labs(x = "Time", y = "Latency [ms]", color = "Percentiles") +
+labs(x = "Czas (hh:mm)", y = "Czas wykonania (ms)", color = "Percentyle") +
 geom_line(aes(y = p50, color = "p50")) +
 geom_line(aes(y = p75, color = "p75")) +
 geom_line(aes(y = p95, color = "p95")) +
-geom_line(aes(y = p99, color = "p99"))
+geom_line(aes(y = p99, color = "p99")) +
+ggtitle("p50, p75, p95, p99 czasu wykonania funkcji")
 
 
