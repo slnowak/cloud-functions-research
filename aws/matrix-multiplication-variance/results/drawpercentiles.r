e2b7fd41c010d24a6014d8ee631e87c77bdc4712
Slawomir Nowak <slawomir.nowak@uk.tesco.com>
2017-08-07 21:04:24 +0200

    variance aws - results

--- aws/matrix-multiplication-variance/results/drawpercentiles.r
+++ aws/matrix-multiplication-variance/results/drawpercentiles.r
@@ -8,7 +8,7 @@ dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
 
 ggplot(data=dat, aes(time)) +
 theme_bw() +
-scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=12)) +
+scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=24)) +
 scale_y_continuous(breaks=pretty_breaks(n=10)) +
 labs(x = "Czas (hh:mm)", y = "Czas wykonania (ms)", color = "Percentyle") +
 geom_line(aes(y = p50, color = "p50")) +
