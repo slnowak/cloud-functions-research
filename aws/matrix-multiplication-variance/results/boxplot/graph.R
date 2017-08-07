ef68d8e0d9c620be82ae23bda7cca53829e9f4a0
Kamil Kulak <kulak.kam@gmail.com>
2017-08-07 23:16:23 +0200

    Fixed labels

--- aws/matrix-multiplication-variance/results/boxplot/graph.R
+++ aws/matrix-multiplication-variance/results/boxplot/graph.R
@@ -1,2 +1,2 @@
 csvData <- read.csv(file="data.csv", header=TRUE, sep=",")
-boxplot(csvData, main="Rozklad czasu odpowiedzi od godziny wykonania testu", ylab="Czas odpowiedzi (ms)", xlab="Czas wykonania testu (hh:mm)", outline=FALSE, las=2, names=c("18:00","19:00","20:00","21:00","22:00","23:00","00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"))
+boxplot(csvData, ylab="Czas odpowiedzi (ms)", xlab="Czas wykonania testu (hh:mm)", outline=FALSE, las=2, names=c("12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","00:00","01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00"))
