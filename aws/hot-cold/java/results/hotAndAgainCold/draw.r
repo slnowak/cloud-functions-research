53c77c9383a1e4e95f1cba368c152c3c12b1e91b
Kamil Kulak <kulak.kam@gmail.com>
2017-08-10 21:06:07 +0200

    hotAndAgainCold

---
+++ aws/hot-cold/java/results/hotAndAgainCold/draw.r
@@ -0,0 +1,15 @@
+library("ggplot2")
+library("scales")
+
+png(filename="aws-coldagain-java.png", width=800, height=600)
+
+dat = read.csv("data.csv")
+dat$timestamp = as.POSIXct(dat$timestamp, format = "%Y-%m-%dT%H:%M:%S")
+
+ggplot(data=dat, aes(timestamp)) +
+geom_line(aes(y = duration)) +
+theme_bw() +
+scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
+scale_y_continuous(breaks=pretty_breaks(n=10)) +
+labs(x = "Czas wykonania zapytania HTTP (hh:mm)", y = "Czas odpowiedzi (ms)")
+
