03b11b99529bb392aebf599264fcbcd7ce781cf7
Slawomir Nowak <slawomir.nowak@uk.tesco.com>
2017-08-30 21:46:48 +0200

    Changed resolution

--- aws/matrix-multiplication/results/drawhosts.r
+++ aws/matrix-multiplication/results/drawhosts.r
@@ -1,7 +1,7 @@
 library("ggplot2")
 library("scales")
 
-png(filename="aws-http-hosts.png", width=640, height=480)
+png(filename="aws-http-hosts.png", width=800, height=600)
 
 dat = read.csv("hosts_aws.csv")
 dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
@@ -10,7 +10,7 @@ ggplot(data=dat, aes(x=time, y=count, group=1)) +
 geom_line(colour="blue", size=0.5) +
    scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
    scale_y_continuous(breaks=pretty_breaks(n=10)) +
-   theme(plot.margin = unit(c(1,1,1,1), "cm")) + theme_bw() +
+   theme_bw(base_size=18) + theme(plot.margin = unit(c(1,1,1,1), "cm")) +
    labs(x="Czas [hh:mm]") +
    labs(y="Liczba hostów") +
    geom_area(aes(y=count))
--- azure/matrix-multiplication/results/drawhosts.r
+++ azure/matrix-multiplication/results/drawhosts.r
@@ -1,17 +1,16 @@
 library("ggplot2")
 library("scales")
 
-png(filename="azure-http-hosts.png", width=640, height=480)
+png(filename="azure-http-hosts.png", width=800, height=600)
 
 dat = read.csv("hosts.csv")
 dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
 
 ggplot(data=dat, aes(x=time, y=count, group=1)) +
-theme_bw() +
-geom_line(colour="black", size=0.5) +
+theme_bw(base_size=18) + theme(plot.margin = unit(c(1,1,1,1), "cm")) +
+geom_line(colour="blue", size=0.5) +
    scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
    scale_y_continuous(breaks=pretty_breaks(n=10)) +
-   theme(plot.margin = unit(c(1,1,1,1), "cm")) +
    labs(x="Czas (hh:mm)") +
    labs(y="Liczba maszyn wirtualnych") +
    geom_area(aes(y=count))
