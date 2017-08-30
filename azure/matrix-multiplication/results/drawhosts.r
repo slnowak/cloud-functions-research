af870173f78d9e40855bbc982a5bdfbab334a61a
Slawomir Nowak <slawomir.nowak@uk.tesco.com>
2017-08-30 22:36:21 +0200

    Changed plots (geom_bar instead of geom_line)

--- azure/matrix-multiplication/results/drawhosts.r
+++ azure/matrix-multiplication/results/drawhosts.r
@@ -6,13 +6,12 @@ png(filename="azure-http-hosts.png", width=800, height=600)
 dat = read.csv("hosts.csv")
 dat$time = as.POSIXct(dat$time, format = "%Y-%m-%dT%H:%M:%SZ")
 
-ggplot(data=dat, aes(x=time, y=count, group=1)) +
+ggplot(data=dat, aes(time, count)) +
+geom_bar(stat="identity") +
 theme_bw(base_size=18) + theme(plot.margin = unit(c(1,1,1,1), "cm")) +
-geom_line(colour="blue", size=0.5) +
-   scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
-   scale_y_continuous(breaks=pretty_breaks(n=10)) +
-   labs(x="Czas (hh:mm)") +
-   labs(y="Liczba maszyn wirtualnych") +
-   geom_area(aes(y=count))
+scale_x_datetime(labels = date_format("%H:%M"), breaks = pretty_breaks(n=10)) +
+scale_y_continuous(breaks=pretty_breaks(n=10)) +
+labs(x="Czas (hh:mm)") +
+labs(y="Liczba maszyn wirtualnych")
 
 
